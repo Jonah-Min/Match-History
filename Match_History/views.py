35bd9ce36a188ec16532e01cfdae255a5b94230d
spoonily <min.jo@husky.neu.edu>
2015-11-17 12:07:31 -0500

    Kiara has a Special Site

--- Match_History/views.py
+++ Match_History/views.py
@@ -51,10 +51,21 @@ def result(request):
 		length = len(recentGames)
 		error = ""
 	elif summoner == "rouged":
-		return render(request, 'Match_History/kiara.html')
+		summonerID = summonerDict[summonerName]['id']
+		summonerIcon = "https://pmcdeadline2.files.wordpress.com/2013/02/minion__130211164715.jpg"
+		recentGames = RiotAPI.getRecentMatches(summonerID)
+		length = len(recentGames)
+		error = ""
+		return render(request, 'Match_History/kiara.html', {'summoner': summoner,
+			'error' : error,
+			'id' : summonerID,
+			'icon' : summonerIcon,
+			'recent' : recentGames,
+			'length' : length,
+			'form' : form})
 	elif summoner == "toona":
 		summonerID = summonerDict[summonerName]['id']
-		summonerIcon = "http://images1.houstonpress.com/imager/u/original/6417352/canned_tuna1.jpg"
+		summonerIcon = "http://freesamples2fillupyourmailbox.com/wp-content/uploads/2015/04/morton.jpg"
 		recentGames = RiotAPI.getRecentMatches(summonerID)
 		length = len(recentGames)
 		error = ""
